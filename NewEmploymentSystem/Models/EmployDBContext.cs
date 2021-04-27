a23cc7ab6e46d656126de82a3c774360b9fbee10
HMD <HMD@DESKTOP-SAFIKLM>
2021-04-27 11:32:37 +0430

    fix bugs

--- NewEmploymentSystem/Models/EmployDBContext.cs
+++ NewEmploymentSystem/Models/EmployDBContext.cs
@@ -46,7 +46,7 @@ namespace NewEmploymentSystem.Models
             if (!optionsBuilder.IsConfigured)
             {
                 //optionsBuilder.UseSqlServer("Server=.;Database=EmployDB;Trusted_Connection=True;");
-                optionsBuilder.UseSqlServer("server=.;database=ShortLinkDB;User Id=sa;Password=S33@||;");
+                optionsBuilder.UseSqlServer("server=.;database=EmployDB;User Id=sa;Password=S33@||;");
             }
         }
 

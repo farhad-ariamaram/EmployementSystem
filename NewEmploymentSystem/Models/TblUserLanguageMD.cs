6f202cea26a0fd2897d324ceddbdc8b3c2f4cd09
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-01 16:25:04 +0430

    add validation for language

---
+++ NewEmploymentSystem/Models/TblUserLanguageMD.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewEmploymentSystem.Models
+{
+
+    public class TblUserLanguageMD
+    {
+
+        [StringLength(1000, ErrorMessage = "طول این فیلد حداکثر 1000 کاراکتر می‌باشد")]
+        public string Description { get; set; }
+    }
+
+
+    [ModelMetadataType(typeof(TblUserLanguageMD))]
+    public partial class TblUserLanguage
+    {
+
+    }
+}

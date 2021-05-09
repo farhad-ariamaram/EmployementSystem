3b8dde4798ecdeb0c15960669c0e1cc7af62b167
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-09 16:28:20 +0430

    fix zero begin of childno and others

--- NewEmploymentSystem/Models/EmployDBContext.cs
+++ NewEmploymentSystem/Models/EmployDBContext.cs
@@ -54,8 +54,7 @@ namespace NewEmploymentSystem.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=EmployDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("server=.;database=EmployDB;User Id=sa;Password=S33@||;");
             }
         }
 

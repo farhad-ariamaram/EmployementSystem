5a5dc5e92afdd8e20484d925f53dfdbb59521bb6
HMD <HMD@DESKTOP-SAFIKLM>
2021-05-10 16:01:18 +0430

    cs to json app settings

--- NewEmploymentSystem/Startup.cs
+++ NewEmploymentSystem/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +25,8 @@ namespace NewEmploymentSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<EmployDBContext>();
-            services.AddDbContext<EmployeeRequestDBContext>();
+            services.AddDbContext<EmployDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CS2")));
+            services.AddDbContext<EmployeeRequestDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CS")));
             services.AddSession();
             services.AddRazorPages();
         }
